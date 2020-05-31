f56f20f8c35ec09ff281f11a3f4453df39ee5399
Anna Lee <annaleeart@yandex.ru>
2020-05-31 14:05:07 +0300

    Created middleware for authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,5 +8,22 @@ const extractBearerToken = (header) => {
 };
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
 
-}
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    return res.status(401).send({ message: 'Необходима авторизация' })ж
+  }
+  //извлечём токен
+  const token = authorization.replace('Bearer', '');
+  let payload;
+
+  try {
+    // попытаемся верифицировать токен
+    payload = jwt.verify(token, 'some-secret-key');
+  } catch (error) {
+    // отправим ошибку, если не получилос
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
+};
