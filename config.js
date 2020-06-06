ef6b7549f2492fc42d2c714d0e6f424a2e6c4426
Anna Lee <annaleeart@yandex.ru>
2020-06-06 20:33:35 +0300

    const { JWT_SECRET, NODE_ENV } = process.env;

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const JWT_SECRET = require('./.env');
+// eslint-disable-next-line no-unused-vars
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports.PrivateKey = process.env.NODE_ENV === 'development' ? 'some-dev-secret' : JWT_SECRET;
